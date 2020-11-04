4df79dd6097a064ab8c40fef54a5b1eb0ec656cd
Valeriy Mikheyev <valeriy.mikheyev@gmail.com>
2020-11-04 18:07:45 +0200

    add scroll

--- Js/app.js
+++ Js/app.js
@@ -1,39 +1,8 @@
  $(function() {
 
- 	var header = $("header"),
- 	    introH = $("#intro").innerHeight(),
- 	    scrolloffset =$(window).scrollTop();
-
-
- 	    /*HEADER fixed*/
- 	chekScroll(scrolloffset);
-
- 	$(window).on("scroll", function(){
-
-
- 		scrolloffset = $(this).scrollTop();
- 		chekScroll(scrolloffset)
-	});
-
-
-
-	function chekScroll(scrolloffset) {
-
- 
- 		if( scrolloffset >= introH){
- 		header.addClass("fixed");
- 		} else{
- 			header.removeClass('fixed');
- 		}
-	}
-
-		/*Smooth scroll*/
-
-
 		$("[data-scroll]").on("click", function(event) {
 
 			event.preventDefault();
-
 			var $this = $(this),
 			blockId = $this.data("scroll"),
 			blockoffset = $(blockId).offset().top;
@@ -46,10 +15,6 @@
 			scrollTop:blockoffset
 			}, 500);
 		});
-
-
-
-
 		/*Nav Toggle*/
 
 		$("#nav_toggle").on("click", function(event) {
@@ -58,42 +23,4 @@
 			$("#nav").toggleClass("active");
 
 		});
-
-		/*HIDE MENU
-
-    	$("#nav a").on("click", function(event){
-        event.preventDefault();
-        $("#nav_toggle").removeClass("active");
-        $("#nav").hide();
-        $("#nav-toggle").removeClass("active");
-
-    });
-  		*/
-
-
-
-		/*COLLAPS*/
-
-		$("[data-collapse]").on("click",function(event){
-			event.preventDefault();
-			var $this = $(this),
-				blockId = $this.data("collapse");
-
-
-			$this.toggleClass("active"); 
-
-
-
-		});
-
-
-		/*SLIDER*/
-
-		$("[data-slider]").slick({
-			infinite:true,
-			fade:false,
-			slidesToShow:1,
-			slidesToScroll:1
-		});
-
  }); 
